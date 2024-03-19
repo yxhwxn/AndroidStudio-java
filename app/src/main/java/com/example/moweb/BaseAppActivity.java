c13e1114fda0c308b55cb00e0d0636fcc19f867d
yxhwxn <officeyh9819@gmail.com>
2024-03-19 17:33:33 +0900

    practice2-4 BaseApp 버튼 추가

--- app/src/main/java/com/example/moweb/BaseAppActivity.java
+++ app/src/main/java/com/example/moweb/BaseAppActivity.java
@@ -1,14 +1,28 @@
 package com.example.moweb;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseAppActivity extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baseapp);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
