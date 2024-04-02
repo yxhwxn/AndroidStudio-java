0ee0a8e5853ee29e488469d786bd8c85daa9ea32
yxhwxn <officeyh9819@gmail.com>
2024-04-02 20:58:24 +0900

    실습 4-2(애완동물 사진보기 앱) 구현

--- app/src/main/java/com/example/moweb/BaseAppActivity.java
+++ app/src/main/java/com/example/moweb/BaseAppActivity.java
@@ -1,27 +1,76 @@
 package com.example.moweb;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseAppActivity extends AppCompatActivity {
 
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+
     Button button1;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_baseapp);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        setTitle("애완동물 사진 보기");
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.Rgroup1);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(chkAgree.isChecked()){
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup1.setVisibility(View.VISIBLE);
+                    btnOK.setVisibility(View.VISIBLE);
+                    imgPet.setVisibility(View.VISIBLE);
+                } else {
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup1.setVisibility(View.INVISIBLE);
+                    btnOK.setVisibility(View.INVISIBLE);
+                    imgPet.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
-                        Toast.LENGTH_SHORT).show();
+                int checkedRadioButtonId = rGroup1.getCheckedRadioButtonId();
+                if (checkedRadioButtonId == R.id.RdoDog) {
+                    imgPet.setImageResource(R.drawable.dog);
+                }
             }
         });
     }
